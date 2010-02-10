991923af113ed65974b5b35bf4eef10a73d87396
Pat Hickey <pat@moreproductive.org>
2010-02-09 20:16:02 -0500

    ignores + get rid of binaries + test file

---
+++ tests/simple.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+char buf[255] = "hello";
+int i;
+/* c style comment */
+// line comment
+/* Multi line c-style comment
+ second line */
+
+int main (void)
+{
+        // first item in body
+        int x;
+        // after int x
+        printf("hello world\n");
+        return 0;
+}
